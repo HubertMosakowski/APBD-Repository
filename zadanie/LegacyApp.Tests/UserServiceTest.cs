d180eda045670d755314a89cb18659c7850bde64
s28829 <s28829@pjwstk.edu.pl>
2024-04-07 22:26:47 +0200

    Zrobione testy jednostkowe dla metody AddUser

--- zadanie/LegacyApp.Tests/UserServiceTest.cs
+++ zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using JetBrains.Annotations;
 using LegacyApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -6,12 +7,50 @@ namespace LegacyApp.Tests;
 
 [TestClass]
 [TestSubject(typeof(UserService))]
-public class UserServiceTest
-{
+public class UserServiceTest {
 
     [TestMethod]
-    public void METHOD()
+    public void AddUser_Should_Return_False_When_User_Has_Credit_Limit_Under_500()
     {
+        string firstName = "Jan";
+        string lastName = "Kowalski";
+        DateTime birthDate = new DateTime(1997, 4, 18);
+        int clientId = 1;
+        string email = "kowalski@wp.pl";
+        var service = new UserService();
+
+        bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
         
+        Assert.AreEqual(false, result);
+    }
+    
+    [TestMethod]
+    public void AddUser_Should_Return_False_When_User_Is_Very_Important_Client()
+    {
+        string firstName = "Mateusz";
+        string lastName = "Malewski";
+        DateTime birthDate = new DateTime(2003, 9, 29);
+        int clientId = 2;
+        string email = "malewski@gmail.com";
+        var service = new UserService();
+
+        bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
+        
+        Assert.AreEqual(false, result);
+    }
+    
+    [TestMethod]
+    public void AddUser_Should_Return_False_When_User_Has_No_Name_Or_Last_Name()
+    {
+        string firstName = "Jan";
+        string lastName = "";
+        DateTime birthDate = new DateTime(1988, 7, 10);
+        int clientId = 1;
+        string email = "Jan@wp.pl";
+        var service = new UserService();
+
+        bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
+        
+        Assert.AreEqual(false, result);
     }
 }
--- zadanie/LegacyApp/UserService.cs
+++ zadanie/LegacyApp/UserService.cs
@@ -1,35 +1,27 @@
 ﻿using System;
 
-namespace LegacyApp
-{
-    public class UserService
-    {
-        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
-        {
+namespace LegacyApp {
+    public class UserService {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId) {
             if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
-            {
                 return false;
-            }
 
             if (!email.Contains("@") && !email.Contains("."))
-            {
                 return false;
-            }
 
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+            
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) 
+                age--;
 
             if (age < 21)
-            {
                 return false;
-            }
 
             var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
 
-            var user = new User
-            {
+            var user = new User {
                 Client = client,
                 DateOfBirth = dateOfBirth,
                 EmailAddress = email,
@@ -37,33 +29,26 @@ namespace LegacyApp
                 LastName = lastName
             };
 
-            if (client.Type == "VeryImportantClient")
-            {
+            if (client.Type == "VeryImportantClient") {
                 user.HasCreditLimit = false;
-            }
-            else if (client.Type == "ImportantClient")
-            {
-                using (var userCreditService = new UserCreditService())
-                {
+            } else if (client.Type == "ImportantClient") {
+                
+                using (var userCreditService = new UserCreditService()) {
                     int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
                     creditLimit = creditLimit * 2;
                     user.CreditLimit = creditLimit;
                 }
-            }
-            else
-            {
+                
+            } else {
                 user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditService())
-                {
+                using (var userCreditService = new UserCreditService()) {
                     int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
                     user.CreditLimit = creditLimit;
                 }
             }
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
                 return false;
-            }
 
             UserDataAccess.AddUser(user);
             return true;
