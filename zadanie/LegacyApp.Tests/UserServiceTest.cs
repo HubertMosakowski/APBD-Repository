592797c1dd8f7a554bc1866f10098d83e9363db2
s28829 <s28829@pjwstk.edu.pl>
2024-04-14 20:11:04 +0200

    93% coverage

--- zadanie/LegacyApp.Tests/UserServiceTest.cs
+++ zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.JavaScript;
 using JetBrains.Annotations;
 using LegacyApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -38,23 +39,9 @@ public class UserServiceTest {
     }
     
     [TestMethod]
-    public void AddUser_Should_Return_False_When_User_Is_Very_Important_Client() {
-        string firstName = "Mateusz";
-        string lastName = "Malewski";
-        DateTime birthDate = new DateTime(2003, 9, 29);
-        int clientId = 2;
-        string email = "malewski@gmail.com";
-        UserService service = new UserService();
-
-        bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
-        
-        Assert.AreEqual(false, result);
-    }
-    
-    [TestMethod]
-    public void AddUser_Should_Return_False_When_User_Has_No_Name_Or_Last_Name() {
-        string firstName = "Jan";
-        string lastName = "";
+    public void AddUser_Should_Return_False_When_User_Has_No_Name() {
+        string firstName = "";
+        string lastName = "Kowalski";
         DateTime birthDate = new DateTime(1988, 7, 10);
         int clientId = 1;
         string email = "Jan@wp.pl";
@@ -90,15 +77,6 @@ public class UserServiceTest {
 
         bool result = service.AddUser(firstName, lastName, email, birthDate, clientId);
         
-        Assert.AreEqual(true, result);
-    }
-    [TestMethod()]
-    public void AddUser_All_Good()
-    {
-        UserService service = new UserService();
-
-        bool result = service.AddUser("Jarosław", "Malewski", "xxx@onlyfans.com", DateTime.Parse("1982-03-21"), 1);
-
         Assert.AreEqual(true, result);
     }
     
